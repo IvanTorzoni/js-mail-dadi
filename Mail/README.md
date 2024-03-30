09e09789659df9383646a291678ceb0c1c706162
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-03-30 18:02:58 +0100

    feat: managment of mail verification

--- Mail/README.md
+++ Mail/README.md
@@ -5,12 +5,12 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 ## Esecuzione Logica
  - [x] Creiamo un bottone e un input mail per l'utente 
- - [ ] Creo l'array contenente le email già registrate 
- - [ ] Collego il pulsante creato nel file HTML
- - [ ] Creo due variabili, una per l'acquisizione del valore inserito dall' utente nell'input e l'altra per il risultato
- - [ ] Aggiungiamo l'evento al bottone
- - [ ] Verifichiamo che la mail inserita dall' utente sia presente nell'array di mail a sistema tramite ciclo for
- - [ ] Gestiamo l'output tramite un ciclo if:
+ - [x] Creo l'array contenente le email già registrate 
+ - [x] Collego il pulsante creato nel file HTML
+ - [x] Creo due variabili, una per l'acquisizione del valore inserito dall' utente nell'input e l'altra per il risultato
+ - [x] Aggiungiamo l'evento al bottone
+ - [x] Verifichiamo che la mail inserita dall' utente sia presente nell'array di mail a sistema tramite ciclo for
+ - [x] Gestiamo l'output tramite un ciclo if:
   - Se la mail inserita da risultato positivo stampa "accesso garantito"
   - altrimenti se la mail non viene trovata stampa "accesso negato. Contattare l'admin"
   - altrimenti se non viene inserito nessuna mail stampa "Inserire mail per verifica" 
--- Mail/js/script.js
+++ Mail/js/script.js
@@ -2,3 +2,39 @@
 // alert("Ciao")
 
 
+// Creo l'array di mail già registrate a sistema
+const arrayEmail = ["test@gmail.com", "test@libero.it", "test@alice.it"]
+
+//Cerco il bottone nella pagina HTML
+const accessBtnElem = document.getElementById("access-btn"); 
+
+// dichiaro le varibili per inserimento della mail e del risultato
+let insertMail;
+let result = false;
+
+// Creo l'evento per il bottone
+accessBtnElem.addEventListener("click", function(){
+    
+    // Salvo le informazioni inserite dall'untente nell' input
+    insertMail = document.getElementById("email").value;
+    result = false;
+
+    // Eseguo il ciclo per la verifica della mail
+    for(let i = 0; i <= arrayEmail.length && result === false && insertMail !== ""; i++){
+        // Se la mail inserita dall'utente è presente nell'array della mail registrate cambia lo stato della variabile result da false a true 
+        if(insertMail === arrayEmail[i] ){
+            result = true;
+        }
+    }
+
+    // Gestione dei messaggi
+    if(result === true){
+        document.getElementById("result").innerHTML = `accesso garantito`;
+    } else if( result === false && insertMail !== ""){
+        document.getElementById("result").innerHTML = `accesso negato. Contattare l'admin`;
+    } else{
+        document.getElementById("result").innerHTML = `Inserire mail per verifica`;
+    }
+    
+    document.getElementById("email").value = "";
+})
